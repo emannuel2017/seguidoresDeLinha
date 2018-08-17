596782c07a34c1d1bc82940c717369de1a5bd7b3
Emannuel <emannuelsouzataguima2016@gmail.com>
2018-08-16 23:35:01 -0300

    mudança de nome de variáveis e adicionados mais duas funções no arquivo startreck1 e renomeado outro arquivo

--- starTrack_teste11/starTrack_teste1.ino
+++ starTrack_teste11/starTrack_teste1.ino
@@ -6,22 +6,20 @@
 #define sen5  7
 
 
-
-
 int sensorPin[5];
 
 int motorA = 3;
 int motorB = 10;
 
-int entradaDireta1 = 4;
-int entradaDireta2 = 5;
-int entradaEsquerda3 = 8;
-int entradaEsquerda4 = 9;
+int Din1 = 4;
+int Din2 = 5;
+int Ein3 = 8;
+int Ein4 = 9;
 
 //int Vel = 100;
 //int brack = 0;
 
-int novaVelocidadeA;
+int novaVelocidadeA; 
 int novaVelocidadeB;
 int buzze = 2;
 
@@ -31,8 +29,8 @@ int espera = 0;
 //int va = 0;
 //int vb = 0;
 
-int centro, direita, esquerda, extremaDireita;
-int extremaEsquerda, esquerdaExterno;
+int centro, direita, esquerda, extremaDireita; 
+int extremaEsquerda;
 
 float kp = 40;
 float ki = 0;
@@ -48,56 +46,45 @@ int entrouNaCurva = 0;
 
 boolean comecoCurva = false;
 
-long tempoInicial = 0l;
-
 void setup() {
   Serial.begin(9600);
 
-  pinMode(entradaDireta1, OUTPUT);
-  pinMode(entradaDireta2, OUTPUT);
-  pinMode(entradaEsquerda3, OUTPUT);
-  pinMode(entradaEsquerda4, OUTPUT);
+  pinMode(Din1, OUTPUT);
+  pinMode(Din2, OUTPUT);
+  pinMode(Ein3, OUTPUT);
+  pinMode(Ein4, OUTPUT);
 
   pinMode(motorA, OUTPUT);
   pinMode(motorB, OUTPUT);
 
   pinMode(buzze, OUTPUT);
-  //  pinMode(sen2, OUTPUT);
-  //  pinMode(sen3, OUTPUT);
-  //  pinMode(sen4, OUTPUT);
-  for (int i = 1000; i < 5000; i++) {
-    tone(buzze, i, 200);
+  
+  for(int i = 1000; i < 5000; i++){ 
+    tone(buzze, i, 200); 
   }
 }
 
-void trocaSinaisDosvaloresAbaixoDeZero(int novaVelocidadeA, int novaVelocidadeB ){
+void frente(int novaVelocidadeA,int novaVelocidadeB) {
 
-   if (novaVelocidadeA < 0) {
+
+  if(novaVelocidadeA < 0){
     novaVelocidadeA = novaVelocidadeA * (-1);
   }
 
 
-  if (novaVelocidadeB < 0) {
+  if(novaVelocidadeB < 0){
     novaVelocidadeB = novaVelocidadeB * (-1);
   }
-  
-  }
-
-
-void frente(int novaVelocidadeA, int novaVelocidadeB) {
-
-trocaSinaisDosvaloresAbaixoDeZero(novaVelocidadeA, novaVelocidadeB);
- 
   /*delay(500);
-    Serial.print("A");
-    Serial.println(novaVelocidadeA);
-    Serial.print("B");
-    Serial.println(novaVelocidadeB);
-  */
-  digitalWrite(entradaDireta1, LOW);
-  digitalWrite(entradaDireta2, HIGH);
-  digitalWrite(entradaEsquerda3, HIGH);
-  digitalWrite(entradaEsquerda4, LOW);
+   Serial.print("A");
+   Serial.println(novaVelocidadeA);
+   Serial.print("B");
+   Serial.println(novaVelocidadeB);
+   */
+  digitalWrite(Din1, LOW);
+  digitalWrite(Din2, HIGH);
+  digitalWrite(Ein3, HIGH);
+  digitalWrite(Ein4, LOW);
 
   analogWrite(motorA, novaVelocidadeA);//78 velocidade minima// 158 velocidade media
   analogWrite(motorB, novaVelocidadeB);//45 velocidade minima // 54 velocidade media---------------------------------------
@@ -113,46 +100,39 @@ void lerSensores() {
   centro = digitalRead(sen3);
   direita = digitalRead(sen4);
   extremaDireita = digitalRead(sen5);
-  esquerdaExterno = analogRead(A5);
-  mudarValorDaLeituraParaUmOuZero();
-}
-void mudarValorDaLeituraParaUmOuZero() {
 
-  if (esquerdaExterno < 500) {
-    esquerdaExterno = 0;
-  } else {
-    esquerdaExterno = 1;
-  }
 
 }
 
 
-void controlePid(int erro) {
 
-  if (comecoCurva) {
+void controlePid(int erro){
+
+  if(comecoCurva){         
     VelocidadeConstatnteA = 60;//+5
     VelocidadeConstatnteB = 60;//+5
-    kp = 15;//15.2//16
-    kd = 15;//5.2//6
-
+      kp = 15;//15.2//16
+      kd = 15;//5.2//6
+         
   }
-  else {
+  else{
     VelocidadeConstatnteA = 60;//+5
     VelocidadeConstatnteB = 60;
-    kp = 20;//15.2//16
-    kd = 20;//5.2//6
+      kp = 20;//15.2//16
+      kd = 20;//5.2//6
+     
   }
 
   /*delay(200);
-    Serial.print("velocidade A");
-    Serial.println(VelocidadeConstatnteA);
-    Serial.print("velocidade B");
-    Serial.println(VelocidadeConstatnteB);*/
+   Serial.print("velocidade A");
+   Serial.println(VelocidadeConstatnteA);
+   Serial.print("velocidade B");
+   Serial.println(VelocidadeConstatnteB);*/
   //+ kd * (erro_anterior - erro)
+  
 
 
-
-  sinalPid = kp * erro + kd * (erro_anterior - erro);
+  sinalPid = kp * erro;// + kd * (erro_anterior - erro);
 
   novaVelocidadeA = VelocidadeConstatnteA + sinalPid ;
   novaVelocidadeB = VelocidadeConstatnteB - sinalPid ;
@@ -160,28 +140,15 @@ void controlePid(int erro) {
   erro_anterior = erro;
 
 
-  frente(novaVelocidadeA, novaVelocidadeB);
+  frente(novaVelocidadeA,novaVelocidadeB);
 
 
 
-}
-
-
-
-long retornaTempoParaOControleDoBuzzer() {
-
-
-  long tempoAtual = millis();
-  if (tempoAtual > (tempoInicial + 500)) {
-    return  tempoInicial = tempoAtual;
-  }
-  return 0l;
 }
 
 void loop() {
 
   lerSensores();
-
 
   //0 1 0 0 0 0 0  --> Erro -4
   //0 1 1 0 0 0 0  --> Erro -3
@@ -197,28 +164,28 @@ void loop() {
 
   //0 0 1 0 0 :0
 
-  if (espera < 1) {
+  if(espera < 1){
     espera++;
     delay(2000);
   }
-  else if ((centro == 1) && (esquerda == 0) && (direita == 0) && (esquerdaExterno == 1) && (extremaDireita == 0) ) {
-
-    if (entrouNaCurva < 1) {
-      Serial.println(entrouNaCurva);
-      comecoCurva = !comecoCurva;
-      tone(buzze, 5000, retornaTempoParaOControleDoBuzzer());
-      entrouNaCurva++;
+  else if ((centro == 1) && (esquerda == 0) && (direita == 0) && (extremaEsquerda == 1) && (extremaDireita == 0) ) {
+    
+    if(entrouNaCurva < 1){ 
+    Serial.println(entrouNaCurva);
+    comecoCurva = !comecoCurva;      
+    tone(buzze, 5000, 200);
+    entrouNaCurva++;
     }
-
+    
   }
 
 
   else if ((centro == 1) && (esquerda == 0) && (direita == 0) && (extremaEsquerda == 0) && (extremaDireita == 1) ) {
-    frente(0, 0);
-    tone(buzze, 2000, retornaTempoParaOControleDoBuzzer());
+    frente(0,0);
+    tone(buzze,2000,500);
   }
 
-  if ( (centro == 1) && (esquerda == 0) && (direita == 0) && (extremaEsquerda == 0) && (extremaDireita == 0)  ) {
+  if ( (centro == 1) && (esquerda == 0) && (direita == 0) && (extremaEsquerda== 0) && (extremaDireita == 0)  ) {
     controlePid(0);
   }
 
@@ -232,48 +199,45 @@ void loop() {
   //0 0 1 1 0 :1
   else if ((centro == 1) && (esquerda == 0) && (direita == 1) && (extremaEsquerda == 0) && (extremaDireita == 0) ) {
     controlePid(1);
-    entrouNaCurva = 0;
+    entrouNaCurva;
     noTone(buzze);
   }
   //0 1 0 0 0 :-2
   else if ((centro == 0) && (esquerda == 1) && (direita == 0) && (extremaEsquerda == 0) && (extremaDireita == 0) ) {
     controlePid(-2);
-    entrouNaCurva = 0;
+    entrouNaCurva;
     noTone(buzze);
   }
 
   //0 0 0 1 0 :2
   else if ((centro == 0) && (esquerda == 0) && (direita == 1) && (extremaEsquerda == 0) && (extremaDireita == 0) ) {
     controlePid(2);
-    entrouNaCurva = 0;
+    entrouNaCurva;
     noTone(buzze);
   }
 
   //1 1 0 0 0 :-3
   else if ((centro == 0) && (esquerda == 1) && (direita == 0) && (extremaEsquerda == 1) && (extremaDireita == 0) ) {
     controlePid(-3);
-    entrouNaCurva = 0;
+    entrouNaCurva;
     noTone(buzze);
   }
 
   //0 0 0 1 1 :3
   else if ((centro == 0) && (esquerda == 0) && (direita == 1) && (extremaEsquerda == 0) && (extremaDireita == 1) ) {
     controlePid(3);
-    entrouNaCurva = 0;
     noTone(buzze);
   }
 
   //1 0 0 0 0 :-4
   else if ((centro == 0) && (esquerda == 0) && (direita == 0) && (extremaEsquerda == 1) && (extremaDireita == 0) ) {
     controlePid(-4);
-    entrouNaCurva = 0;
     noTone(buzze);
   }
 
   //0 0 0 0 1 :4
   else if ((centro == 0) && (esquerda == 0) && (direita == 0) && (extremaEsquerda == 0) && (extremaDireita == 1) ) {
     controlePid(4);
-    entrouNaCurva = 0;
     noTone(buzze);
   }
 
